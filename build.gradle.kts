c6f49cfe183538fb0569492756765618c8683dcb
Ilya Mikheev <i.d.mikheev@tinkoff.ru>
2023-02-09 23:00:56 +0300

    add license and changed dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,44 @@
+/*
+
+MIT License
+
+Copyright (c) 2023 Ilya Mikheev
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ */
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
     id("org.jetbrains.kotlin.plugin.spring") version "1.7.10"
-    id("org.springframework.boot") version "2.7.8"
     id("io.spring.dependency-management") version "1.1.0"
     `maven-publish`
+    `java-library`
 //    signing
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.boot:spring-boot-dependencies:2.7.8")
+    }
 }
 
 group = "com.github.mikheevshow"
@@ -17,11 +49,14 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    api("io.micrometer:micrometer-registry-prometheus:1.10.3")
+
+    compileOnly("org.springframework.boot:spring-boot")
+    compileOnly("org.springframework.boot:spring-boot-autoconfigure")
+    compileOnly("javax.annotation:javax.annotation-api:1.3.2")
+    compileOnly("org.slf4j:slf4j-api:2.0.6")
+    compileOnly("io.micrometer:micrometer-registry-prometheus:1.10.3")
 
     testImplementation(kotlin("test"))
-
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
     testApi("org.junit.jupiter:junit-jupiter-engine:5.9.2")
     testImplementation("io.mockk:mockk:1.13.4")
@@ -38,6 +73,11 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "1.8"
+    targetCompatibility = "1.8"
 }
 
 java {
