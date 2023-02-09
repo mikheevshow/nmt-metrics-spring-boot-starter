c1fc6dc8fe190e20b77cfed2e7c4741e3af68e21
Ilya Mikheev <i.d.mikheev@tinkoff.ru>
2023-02-09 21:07:40 +0300

    add ci and gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,11 @@ plugins {
     id("org.jetbrains.kotlin.plugin.spring") version "1.7.10"
     id("org.springframework.boot") version "2.7.8"
     id("io.spring.dependency-management") version "1.1.0"
+    `maven-publish`
+    signing
 }
 
-group = "org.example"
+group = "com.github.mikheevshow"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -26,4 +28,9 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
